a5b3aa2d95995a5b601092800df98d5219635dd9
reinel215 <reinelencuestas@gmail.com>
2020-05-06 08:50:54 -0400

    se agrego la alerta de cerrar sesion y la opcion en el side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public alertController: AlertController,
+    private router : Router
   ) {
     this.initializeApp();
     this.sideMenu();
@@ -54,6 +57,32 @@ export class AppComponent {
         icon : "person-outline"
       }
     ]
+  }
+
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Cerrar sesion',
+      message: 'Estas seguro que deseas cerrar la sesión?',
+      buttons: [
+        {
+          text: 'Quedarme',
+          role: 'cancel'
+        }, {
+          text: 'Cerrar',
+          handler: () => {
+            this.router.navigate(['/login']);
+          }
+        }
+      ]
+    })
+    await alert.present();
+  }
+
+
+  cerrarSesion(){
+    this.presentAlertConfirm();
+
   }
 
 }
